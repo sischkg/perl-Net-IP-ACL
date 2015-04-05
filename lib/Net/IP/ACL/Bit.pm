3cf291e40537aa373b76b94c3ccb3db4de6c4715
Toshifumi Sakaguchi <sischkg@gmail.com>
2015-04-05 22:26:50 +0900

    Bit/Exceptionのドキュメントを追加

--- lib/Net/IP/ACL/Bit.pm
+++ lib/Net/IP/ACL/Bit.pm
@@ -16,6 +16,66 @@ has 'acl_entry' => ( isa     => 'Maybe[Net::IP::ACL::Entry]',
 		     is      => 'rw',
 		     default => undef );
 
+=head1 NAME
+
+Net::IP::ACL::Bit - Node of binary tree which represents ACL.
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+    use Net::IP::ACL::Bit;
+
+    # network 01100000 00000000 00000000 00000000 = 96.0.0.0/8
+    # netmask 11111111 00000000 00000000 00000000
+    my $top = new Net::IP::ACL::Bit( bit = 0 );
+    my $bit_0th = new Net::IP::ACL::Bit( bit => 0 ); # 0
+    my $bit_1st = new Net::IP::ACL::Bit( bit => 1 ); # 1
+    my $bit_2nd = new Net::IP::ACL::Bit( bit => 1 ); # 1
+    my $bit_3rd = new Net::IP::ACL::Bit( bit => 0 ); # 0
+    my $bit_4th = new Net::IP::ACL::Bit( bit => 0 ); # 0
+    my $bit_5th = new Net::IP::ACL::Bit( bit => 0 ); # 0
+    my $bit_6th = new Net::IP::ACL::Bit( bit => 0 ); # 0
+    my $bit_7th = new Net::IP::ACL::Bit( bit => 0 ); # 0
+
+    $top->next_0( $bit_0th );     # 0
+    $bit_0th->next_1( $bit_1st ); # 1
+    $bit_1st->next_1( $bit_2nd ); # 1
+    $bit_2nd->next_0( $bit_3rd ); # 0
+    $bit_3rd->next_0( $bit_4th ); # 0
+    $bit_4rd->next_0( $bit_5th ); # 0
+    $bit_5rd->next_0( $bit_6th ); # 0
+    $bit_6rd->next_0( $bit_7th ); # 0
+
+
+=head1 SUBROUTINES/METHODS
+
+=head2
+
+=head2 new( bit => $bit )
+
+=head2 next_0( $bit )
+
+set 0-node child.
+
+=head2 next_0()
+
+get 0-node child.
+
+=head2 next_1( $bit )
+
+set 1-node child.
+
+=head2 next_1()
+
+get 1-node child.
+
+=head2 print_acl_bit()
+
+print debug message to STDERR.
+
+=cut
+
 sub print_acl_bit {
     my $this = shift;
 
--- lib/Net/IP/ACL/Exception.pm
+++ lib/Net/IP/ACL/Exception.pm
@@ -1,6 +1,41 @@
 
 package Net::IP::ACL::Exception;
 
+=head1 NAME
+
+Net::IP::ACL::Expetion - Exepction classes for Net::IP::ACL.
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+    use English;
+    use Net::IP::ACL::Exeption;
+
+    eval {
+        Net::IP::ACL::ArgumentError->throw(
+            error_message => "invalid argument.".
+        );
+    };
+    if ( $err = $EVAL_ERROR ) {
+       printf STDERR "error: %s\n", $err;
+    }
+
+
+=head1 SUBROUTINES/METHODS
+
+=head2
+
+=head2 throw( error_message => $error_message )
+
+Throw exeption.
+
+=head2 full_message
+
+return error message.
+
+=cut
+
 use Exception::Class (
 	'Net::IP::ACL::ArgumentError' => {
 		fields => [ 'error_message' ],
